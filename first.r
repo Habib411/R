f51a2ab982a6549c0ed2084ec300caf18aed0f5e
Habib411 <tradingworld555@gmail.com>
2021-01-19 21:58:35 +0500

    add factor method

--- first.r
+++ first.r
@@ -1,2 +1,23 @@
 # This is my first repository
 print("Hello R!")
+m <- matrix(1:5,nrow = 5,ncol = 5)
+
+m
+
+dim(m)
+
+attributes(m)
+
+dim(m) <- c(2,3)
+
+m <- 1:6
+
+dim(m)
+
+m
+a <- 1:5
+b <- 11:15
+cbind(a,b)
+
+rbind(a,b)
+
