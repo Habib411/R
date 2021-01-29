ad367c370ad091cf68e69cab48c94182d54833a6
Habib411 <tradingworld555@gmail.com>
2021-01-29 21:26:03 +0500

    Date and time

--- first.r
+++ first.r
@@ -319,3 +319,74 @@ x <- matrix(1:4, 2, 2); y <- matrix(rep(10,4,),2,2)
 x*y     # element-wise multiplication [,1] [,2]
 x/y
 x%*%y   #true matrix multiplication
+
+# Dates and Times in R
+
+# R has developed a special representation of dates and times:
+# Dates are represented by Date class.
+# Times are represented by the POSIXct or the POSIXlt class.e
+# Date are stored internally as the number of days since 1970-01-01.
+# Times are stored internally as the number of seconds since 1970-01-01.
+
+as.Date("2021-01-29")
+as.POSIXct("2021-01-29")
+as.POSIXlt("2021-01-29")
+
+# Times in R
+# Times are represented using th POSIXct or the POSIXlt class
+# POSIXct is just a very large integer under the hood; it use a useful class when you want to store times in something like a data frame.
+# POSIXlt is a list underneath and it stores a bunch of other useful information like the day of the week, day of the year, month, day of the month.
+# There are a few generic functions that work on dates and times
+# weekdays: give the day of the week.
+# months: give the month name.
+# quarters: give the quarter number ("Q1","Q2","Q3","Q4")
+
+# Times in R (Contd.)
+# Times can be coerced from a character string using the as.POSIXlt or as.POSIXct function.
+
+x <- Sys.time()
+x
+p <- as.POSIXlt(x)
+p
+names(unclass(p))
+p$sec
+p$mon
+p$wday
+
+# You can also use the POSIXct format.
+
+x <- Sys.time()
+x    # # Already in 'POSIXct' format
+x$sec
+p <- as.POSIXlt(x)
+p$sec
+
+# Finally, there is the strptime function in case your dates are written in a different format
+
+datestring <- c("January 10, 2012 10:40","December 9, 2011 9:10")
+x <- strptime(datestring, "%B %d, %Y %H:%M")
+x
+class(x)
+
+# I can never remember the formatting strings. Check ?strptime for details.
+
+# You can use mathematical operation on dates and times. Well, really just addition (+) and subtraction (-). You can do comparisons too(i.e.==, <=).
+ 
+x <- as.Date("2021-01-29")
+y <- strptime("9 Jan 2011 11:34:21","%d %b %Y %H:%M:%S")
+x-y
+class(x)
+class(y)
+x <- as.POSIXlt(x)
+x-y
+
+# Even keeps track of leap years, leap seconds, daylight savings, and time zones.
+
+x <- as.Date("2012-04-01")
+y <- as.Date("2012 -01-28")
+x
+y
+x-y
+x <- as.POSIXct("2012-10-25 01:00:00")
+y <- as.POSIXct("2012-10-25 03:00:00", tz = "GMT")
+x-y
